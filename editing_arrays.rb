0eafff15745cfcb6c774660a31d48b2bc1cf56a1
ctyrrell0512 <colleen.a.tyrrell@gmail.com>
2018-06-05 22:30:02 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -5,7 +5,9 @@ rainbow_colors = [
 ]
 
 def change_rainbow_colors
-  # Write a solution that corrects the three colors in rainbow_colors, then returns the corrected array
+  rainbow_colors
+  puts room[0] = "red"
+  puts 
 end
 
 def add_colors
