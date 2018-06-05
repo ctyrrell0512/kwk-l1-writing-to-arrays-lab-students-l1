92143d5d1dd09a1579cb8d964a46826ffbde1396
ctyrrell0512 <colleen.a.tyrrell@gmail.com>
2018-06-05 22:33:10 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -6,10 +6,16 @@ rainbow_colors = [
 
 def change_rainbow_colors
   rainbow_colors
-  puts room[0] = "red"
-  puts 
+  puts rainbow_colors[0] = "red"
+  puts rainbow_colors[1] = "light red"
+  puts rainbow_colors[2] = "light yellow"
 end
+change_rainbow_colors
 
 def add_colors
-  # Write a solution that adds "green", then "blue" to the rainbow_colors array, then returns the array
+  rainbow_colors
+  puts rainbow_colors.push"green"
+  puts rainbow_colors.push "blue"
 end
+
+add_colors
